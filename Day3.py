dffb7b8d6f4dbef3abc8933e1dcf6031a6ee48d1
Rok Zabret <71169333+RokZ999@users.noreply.github.com>
2022-12-18 14:05:11 +0100

    Day3

---
+++ Day3.py
@@ -0,0 +1,18 @@
+def findDuplicateChar(h1, h2):
+    for indexh1 in range(len(h1)):
+        for indexh2 in range(len(h2)):
+            if h1[indexh1] == h2[indexh2]:
+                return h1[indexh1]
+    return ""
+
+
+def priorityCalculator(ch):
+    return ord(ch)-96 if ord(ch) > 94 else ord(ch)-38
+
+
+f = open("inputD3.txt", "r")
+sum = 0
+
+for x in f:
+    sum += priorityCalculator(findDuplicateChar(x[len(x)//2:], x[:len(x)//2]))
+print(sum)
